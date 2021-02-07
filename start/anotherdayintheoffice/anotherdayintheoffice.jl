752a1e5d9cab67deb533ef9d0ac2f305ba920ce7
Kahlil Wehmeyer <kahlilwehmeyer@pm.me>
2021-02-07 14:49:21 -0500

    another day in the office

---
+++ start/anotherdayintheoffice/anotherdayintheoffice.jl
@@ -0,0 +1,49 @@
+## 
+using Luxor, Colors
+
+## vars
+canvas_width = paper_sizes["A4"][2]
+canvas_height = paper_sizes["A4"][1]
+
+bg_color = colorant"#376B8C"
+ln_color = colorant"#B4D2D9"
+ln_color2 = colorant"#BF8821"
+border_color = colorant"#B5BF6B"
+
+##
+
+Drawing(canvas_height,canvas_width, :png)
+origin()
+background(bg_color)
+
+tiles = Tiler(canvas_height,canvas_width, 4,3, margin = 10)
+
+for (pos, n) in tiles
+    sethue(ln_color)
+    gsave()
+    translate(pos)
+    if rand(1:16) == n 
+        #sethue(box_color)
+        #box(pos, tiles.tilewidth, tiles.tileheight, :stroke)
+        sethue(ln_color2)
+        p = [Point(rand(-tiles.tileheight/3:tiles.tileheight/3), rand(-tiles.tileheight/3:tiles.tileheight/3)) for i in 1:rand(5:20)]
+        pp = poly(p, :stroke)
+        grestore()
+        continue
+    end
+
+    p = [Point(rand(-tiles.tileheight/3:tiles.tileheight/3), rand(-tiles.tileheight/3:tiles.tileheight/3)) for i in 1:rand(5:20)]
+    pp = poly(p, :stroke)
+    polysmooth(pp, 100, :stroke)
+    grestore()
+
+end
+
+setline(40)
+sethue(border_color)
+box(O, canvas_height, canvas_width, :stroke)
+
+finish()
+preview() 
+
+##
